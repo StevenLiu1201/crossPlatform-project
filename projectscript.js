7da0db964a7151c0b4a08c36589fe5f6445744db
Ka Kei Cheung <cheu0095@algonquinlive.com>
2023-11-14 11:15:44 -0500

    updates

--- projectscript.js
+++ projectscript.js
@@ -7,55 +7,55 @@ const options = {
 		'X-RapidAPI-Key': 'bbda02016amshccb22789404656cp117221jsn2390d8e2f0c8',
 		'X-RapidAPI-Host': 'dogs-by-api-ninjas.p.rapidapi.com'
 	}
-};
+}
 
 async function getDog() {
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-    return result;
+    const response = await fetch(url, options)
+    const result = await response.text()
+    console.log(result)
+    return result
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-const urlAll = 'https://dogbreeds.p.rapidapi.com/api/dog-breeds/list/profiles';
+const urlAll = 'https://dogbreeds.p.rapidapi.com/api/dog-breeds/list/profiles'
 const optionsAll = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'bbda02016amshccb22789404656cp117221jsn2390d8e2f0c8',
 		'X-RapidAPI-Host': 'dogbreeds.p.rapidapi.com'
 	}
-};
+}
 
 async function getAll() {
   try {
-    const response = await fetch(urlAll, optionsAll);
-    const result = await response.text();
-    console.log(result);
+    const response = await fetch(urlAll, optionsAll)
+    const result = await response.text()
+    console.log(result)
     return result
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-const urlone = 'https://dogbreeds.p.rapidapi.com/api/dog-breeds/shiba-inu';
+const urlone = 'https://dogbreeds.p.rapidapi.com/api/dog-breeds/shiba-inu'
 const optionsone = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'bbda02016amshccb22789404656cp117221jsn2390d8e2f0c8',
 		'X-RapidAPI-Host': 'dogbreeds.p.rapidapi.com'
 	}
-};
+}
 
 async function getOne() {
   try {
-    const response = await fetch(urlone, optionsone);
-    const result = await response.text();
-    console.log(result);
+    const response = await fetch(urlone, optionsone)
+    const result = await response.text()
+    console.log(result)
     return result
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
