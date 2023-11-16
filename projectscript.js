f78480d8be19e63def98913c2412589d8677fdb6
Zhongrui Liu <liu00642@algonquinlive.com>
2023-11-16 16:52:07 -0500

    update getAllBreeds

--- projectscript.js
+++ projectscript.js
@@ -3,17 +3,17 @@
 // dynamic url
 function buildBreedUrl(parameter,value,offset){
   if (offset){
-    return 'https://dogs-by-api-ninjas.p.rapidapi.com/v1/dogs?'+parameter+'='+value+'&offset='+offset;
+    return 'https://api.api-ninjas.com/v1/dogs?'+parameter+'='+value+'&offset='+offset;
   }else{
-    return 'https://dogs-by-api-ninjas.p.rapidapi.com/v1/dogs?'+parameter+'='+value
+    return 'https://api.api-ninjas.com/v1/dogs?'+parameter+'='+value
   }
   
 }
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'bbda02016amshccb22789404656cp117221jsn2390d8e2f0c8',
-		'X-RapidAPI-Host': 'dogs-by-api-ninjas.p.rapidapi.com'
+		'X-Api-Key': '8Xubu5U6AnvH7AhkVhgv0rdGMjkUZfXAf0zCS89e',
+		//'X-RapidAPI-Host': 'dogs-by-api-ninjas.p.rapidapi.com'
 	}
 }
 
@@ -28,20 +28,57 @@ async function getDogbyName_breeds(value,parameter='name') {
 }
 //getDogbyName_breeds('golden%20retriever')
 
+
 async function getDogs_breeds(value,parameter,offset) {
   try {
-    console.log(buildBreedUrl(parameter,value,offset));
     const response = await fetch(buildBreedUrl(parameter,value,offset), options)
     const result = await response.json()
-    console.log(result)
     return result
   } catch (error) {
     console.error(error)
+    return null;
   }
 }
-getDogs_breeds(3,'barking',20)
+//getDogs_breeds(3,'barking',20)
 
 
+//getAllBreeds()
+async function getAllBreeds() {
+  let allBreeds = [];
+
+  for (let i = 1; i <= 5; i++) {
+
+    let done = true;
+    let offset = 20;
+    let num = 1
+
+    while (done) {
+      const result = await getDogs_breeds(i, 'barking', offset);
+      console.log(result);
+
+      if (!result || result.length === 0) {
+        done = false;
+      } else {
+        allBreeds.push(result);
+        offset = offset + 20;
+      }
+      // num is for protect infinit looping 
+      num = num +1
+      if(num >2){
+        done = false
+      }
+
+    }
+  }
+  return allBreeds;
+}
+
+// Call the function to make the API calls
+/*
+getAllBreeds().then(result => {
+  console.log("All breeds:", result);
+});
+*/
 
 const urlAll = 'https://dogbreeds.p.rapidapi.com/api/dog-breeds/list/profiles'
 const optionsAll = {
--- team members.md
+++ team members.md
@@ -9,6 +9,10 @@ frontend developer: Steven
 
 API: dogs
 https://api-ninjas.com/api/dogs
+key:
+HT :  9ERsgSi5iBhEcYiI0G5C1w==HyJBiKD5YXlHlKH7
+Steven : 8Xubu5U6AnvH7AhkVhgv0rdGMjkUZfXAf0zCS89e
+jacky : bbda02016amshccb22789404656cp117221jsn2390d8e2f0c8
 
 API: All dogs breeds
 https://dogbreeds.p.rapidapi.com/api/dog-breeds/list/profiles
